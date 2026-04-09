ff62893ca234e74a9f858fc28ff84837457623fc
jinseunghyeon <jjin9050@naver.com>
2026-04-09 17:58:44 +0900

    pdf 가져오기 함수

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import fs from 'node:fs'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import getPDF from './parse/getPDF.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// PDF 파일을 읽어서 buffer로 변환
+const buffer = fs.readFileSync(join(__dirname, './pdf-files/2026년도+4월+16일+시행+전기요금표(종합).pdf'))
+
+const pdf = await getPDF(buffer)
---
+++ src/parse/getPDF.ts
@@ -0,0 +1,25 @@
+import { getDocument, type PDFDocumentProxy } from 'pdfjs-dist/legacy/build/pdf.mjs'
+
+const PDF_SIGNATURE = '%PDF'
+
+export default async function getPDF(buffer: Buffer): Promise<PDFDocumentProxy | null> {
+    
+    // PDF 파일의 시그니처를 확인하여 유효한 PDF 파일인지 검사
+    const signature = buffer.subarray(0, 4).toString('utf-8')
+
+    if (signature === PDF_SIGNATURE) {
+        console.log(`PDF 크기: ${buffer.length} bytes`)
+    } else {
+        console.log('PDF 파일이 아닙니다.')
+        return null
+    }
+
+    const uint8Array = new Uint8Array(buffer)
+
+    const loadingTask = getDocument(uint8Array)
+    const pdf = await loadingTask.promise
+
+    console.log(`PDF 페이지 수: ${pdf.numPages}`)
+
+    return pdf
+}
